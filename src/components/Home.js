eadc9431061051b51ec90802e8c15ac5d443876d
aerocanlas <princeaero.canlas.cics@ust.edu.ph>
2024-03-08 15:05:20 +0800

    home carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -28,7 +28,7 @@ export default function Home() {
   <div id="slide1" className="carousel-item relative w-full">
     <img src="./images/slides/1.jpg" className="w-full object-cover object-center opacity-60" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide4" className="btn btn-circle">❮</a> 
+      <a href="#slide5" className="btn btn-circle">❮</a> 
       <a href="#slide2" className="btn btn-circle">❯</a>
     </div>
   </div> 
@@ -47,9 +47,16 @@ export default function Home() {
     </div>
   </div> 
   <div id="slide4" className="carousel-item relative w-full">
-  <img src="./images/slides/4.jpg" className="w-full object-cover object-center " />
+  <img src="./images/slides/4.jpg" className="w-full object-cover object-center opacity-60" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide3" className="btn btn-circle">❮</a> 
+      <a href="#slide5" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide5" className="carousel-item relative w-full">
+  <img src="./images/slides/5.jpg" className="w-full object-cover object-center opacity-60 " />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" className="btn btn-circle">❮</a> 
       <a href="#slide1" className="btn btn-circle">❯</a>
     </div>
   </div>
